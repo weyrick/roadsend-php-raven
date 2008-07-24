0792a65d96b094328904ede7095e5f9a2723c537
Shannon Weyrick <weyrick@mozek.us>
2008-07-24 15:52:14 +0000

    parse commandline options

--- frontend/cli/main.cpp
+++ frontend/cli/main.cpp
@@ -1,13 +1,45 @@
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <boost/program_options.hpp>
 #include "pDriver.h"
+
+namespace po = boost::program_options;
 
 int main( int argc, char* argv[] )
 {
 
     rphp::pDriver driver;
+
+    //int opt;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+//        ("optimization", po::value<int>(&opt)->default_value(10), "optimization level")
+        ("input-file", po::value< std::vector<std::string> >(), "input file")
+        ;
+
+    po::positional_options_description p;
+    p.add("input-file", -1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).
+            options(desc).positional(p).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help") || !vm.count("input-file")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
     
     std::cout << "Roadsend PHP" << std::endl;
-    driver.compile("fiddle.php");
 
+    std::vector<std::string> infiles = vm["input-file"].as< std::vector<std::string> >();
+    for (std::vector<std::string>::iterator it = infiles.begin(); it!=infiles.end(); ++it) {
+        driver.compile(*it);
+    }
+
+    //std::cout << "Optimization level is " << opt << "\n";
+ 
 }
