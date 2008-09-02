1bcc0b3af8e3bce1150a4e96e005074c1b93f43c
Shannon Weyrick <weyrick@mozek.us>
2008-09-02 08:23:35 +0000

    little test program for determining sizes

---
+++ runtime/test/rphp-sizes.cpp
@@ -0,0 +1,24 @@
+
+#include "pRuntime.h"
+
+using namespace rphp;
+
+int main(void) {
+
+    std::cout << "pTriState: " << sizeof(pTriState) << std::endl;
+    std::cout << "pInt: " << sizeof(pInt) << std::endl;
+    std::cout << "pFloat: " << sizeof(pFloat) << std::endl;
+    std::cout << "pBString: " << sizeof(pBString) << std::endl;
+    std::cout << "pUString: " << sizeof(pUString) << std::endl;
+    std::cout << "pUStringP: " << sizeof(pUStringP) << std::endl;
+    std::cout << "pHash: " << sizeof(pHash) << std::endl;
+    std::cout << "pHashP: " << sizeof(pHashP) << std::endl;
+    std::cout << "pObject: " << sizeof(pObject) << std::endl;
+    std::cout << "pObjectP: " << sizeof(pObjectP) << std::endl;
+    std::cout << "pResource: " << sizeof(pResource) << std::endl;
+    std::cout << "pResourceP: " << sizeof(pResourceP) << std::endl;
+    std::cout << "pVarRef: " << sizeof(pVarP) << std::endl;
+    std::cout << "pVar: " << sizeof(pVar) << std::endl;
+
+}
+ 
