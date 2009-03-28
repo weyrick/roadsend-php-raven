9532c38ce434ed80d0ef4435eb741a0d03c90f47
Shannon Weyrick <weyrick@mozek.us>
2009-03-28 08:18:04 +0000

    some simple testing of pVar create/assign speed

---
+++ runtime/test/rphp-pVar-speed.cpp
@@ -0,0 +1,93 @@
+
+#include "rphp/runtime/pRuntime.h"
+
+using namespace rphp;
+
+void testScalar(int reps) {
+
+    pVar foo;
+    for(int i = 0; i < reps; i++) {
+        foo = pNull;
+        foo = pTrue;
+        foo = 5;
+        foo = 5.5;
+    }
+
+}
+
+void testBString(int reps) {
+
+    pVar foo;
+    for(int i = 0; i < reps; i++) {
+        foo = pBString("foo");
+        foo = pNull;
+    }
+
+}
+
+void testBStringLong(int reps) {
+    
+    pVar foo;
+    for(int i = 0; i < reps; i++) {
+        foo = pBString("012345678901234567890123456789");
+        foo = pNull;
+    }
+
+}
+
+void testUString(int reps) {
+    
+    pVar foo;
+    for(int i = 0; i < reps; i++) {
+        foo = pUStringP("foo");
+        foo = pNull;
+    }
+
+}
+
+void testUStringLong(int reps) {
+    
+    pVar foo;
+    for(int i = 0; i < reps; i++) {
+        foo = pUStringP("012345678901234567890123456789");
+        foo = pNull;
+    }
+
+}
+
+void testHashEmpty(int reps) {
+    
+    pVar foo;
+    for(int i = 0; i < reps; i++) {
+        foo.newEmptyHash();
+        foo = pNull;
+    }
+
+}
+
+void testHash(int reps, int size) {
+    
+    pVar foo;
+    for(int i = 0; i < reps; i++) {
+        foo.newEmptyHash();
+        for (int j = 0; j < size; j++) {
+            foo.getHash()->insert(j,42);
+        }
+        foo = pNull;
+    }
+
+}
+
+int main(void) {
+
+    int reps=500000;
+    testScalar(reps);
+    testBString(reps);
+    testBStringLong(reps);
+    testUString(reps);
+    testUStringLong(reps);
+    testHashEmpty(reps);
+    testHash(reps, 5);
+
+}
+ 
---
+++ runtime/test/rphp-pVar-speed.php
@@ -0,0 +1,17 @@
+<?
+
+// test assignment speed
+for($i = 0; $i < 5000000; $i++) {   
+    $foo = null;
+    $foo = true;
+    $foo = 5;
+    $foo = 5.5;
+    $foo = b"foo";
+    /*    
+    $foo = array();
+    $foo[5] = 5;
+    $foo = "foo";
+    */
+}
+
+?>
