065dffce45a7b5c84cf8891997727e3d74f3d39b
Shannon Weyrick <weyrick@mozek.us>
2009-08-17 20:02:39 +0000

    a new sandbox for testing/debugging. first up are c++ exceptions in llvm ir

---
+++ sandbox/cpp-exception/cpp-jit-catcher.cpp
@@ -0,0 +1,86 @@
+/* ***** BEGIN LICENSE BLOCK *****
+;; Roadsend PHP Compiler
+;;
+;; Copyright (c) 2009 Shannon Weyrick <weyrick@roadsend.com>
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+   ***** END LICENSE BLOCK *****
+*/
+
+#include <llvm/Module.h>
+#include <llvm/System/DynamicLibrary.h>
+#include <llvm/ModuleProvider.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Target/TargetOptions.h>
+#include <llvm/ExecutionEngine/JIT.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+
+#include <iostream>
+#include <string>
+
+using namespace llvm;
+
+
+// TODO: generate IR that calls into the mock runtime lib
+// then execute it inside of a c++ try/catch to verify exceptions
+
+/*
+void execute(void) {
+
+    // we need exception handling for our c++ runtime to work properly
+    // therefore we tell llvm JIT to generate the proper dwarf tables
+    llvm::ExceptionHandling = true;
+
+    ExistingModuleProvider* MP = new ExistingModuleProvider(llvmModule_);
+
+    std::string errMsg;
+    ExecutionEngine* EE = ExecutionEngine::createJIT(MP, &errMsg);
+    if (!EE) {
+        std::cerr << errMsg << std::endl;
+        return;// false;
+    }
+
+    // EE now owns MP and M
+    Function* main = MP->getModule()->getFunction(entryFunction_);
+
+    if (!main) {
+        std::cerr << "error: entry symbol not found: " << entryFunction_ << std::endl;
+        delete EE;
+        return;// false;
+    }
+
+    EE->runStaticConstructorsDestructors(false);
+
+    // JIT magic
+    //pRuntimeEngine* r = new pRuntimeEngine();
+    void *mainPtr = EE->getPointerToFunction(main);
+//    std::cerr << "found main function at: " << mainPtr << std::endl;
+    // cast to entry function type (returns void, takes one parameter of runtime engine instance)
+    // see pGenerator::createEntryPoint, pIRHelper::moduleEntryFunType
+    void (*mainFunc)(pVar*,pRuntimeEngine*) = (void (*)(pVar*,pRuntimeEngine*))mainPtr;
+    pVar retVal;
+    mainFunc(&retVal, runtime_);
+
+    EE->runStaticConstructorsDestructors(true);
+
+    // FIXME: leak?
+    //delete EE;
+    //delete r;
+
+    //return true;
+
+}
+
+*/
---
+++ sandbox/cpp-exception/cpp-lib.cpp
@@ -0,0 +1,41 @@
+/* ***** BEGIN LICENSE BLOCK *****
+;; Roadsend PHP Compiler
+;;
+;; Copyright (c) 2009 Shannon Weyrick <weyrick@roadsend.com>
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+   ***** END LICENSE BLOCK *****
+*/
+
+
+#include "cpp-lib.h"
+
+#include <iostream>
+#include <stdexcept>
+
+mockRuntimeLib::mockRuntimeLib(void) {
+    std::cout << "mockRuntimeLib initialized" << std::endl;
+}
+
+mockRuntimeLib::~mockRuntimeLib(void) {
+    std::cout << "mockRuntimeLib destroyed" << std::endl;
+}
+
+void mockRuntimeLib::throwError(const std::string& msg) {
+
+    throw std::runtime_error(msg);
+
+}
+
