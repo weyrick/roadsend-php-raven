b3979636cab4f4029e0935f4ab3430fed3276a24
Shannon Weyrick <weyrick@mozek.us>
2009-11-15 21:18:01 +0000

    cpp

--- compiler/analysis/lemon/lemon.c
+++ compiler/analysis/lemon/lemon.c
@@ -3566,7 +3566,7 @@ int mhflag;     /* Output in makeheaders format if true */
 
   in = tplt_open(lemp);
   if( in==0 ) return;
-  out = file_open(lemp,".c","wb");
+  out = file_open(lemp,".cpp","wb");
   if( out==0 ){
     fclose(in);
     return;
